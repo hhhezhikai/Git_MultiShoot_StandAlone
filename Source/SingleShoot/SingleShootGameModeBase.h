92b2bd483995fac9aafb8a5b8bcfb67742fa72fc
‘HeZhiKai’ <he18607794620@qq.com>
2024-02-17 02:22:36 +0800

    创建项目

---
+++ Source/SingleShoot/SingleShootGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SingleShootGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SINGLESHOOT_API ASingleShootGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
